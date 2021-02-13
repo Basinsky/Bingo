dc4fb64d0a047d9470caf6c15c3346210c86c659
Basinsky <51274563+Basinsky@users.noreply.github.com>
2021-02-13 23:08:54 +0100

    update model

--- bingoSpawner.js
+++ bingoSpawner.js
@@ -5,7 +5,7 @@ var TIME_OUT_MS = 1000;
 var nummerID = Entities.addEntity({
     type: "Model",        
     name: "BingoMain",
-    modelURL: LOCATION_ROOT_URL + "loom16.fbx?"+ Date.now(),
+    modelURL: LOCATION_ROOT_URL + "loom17.fbx?"+ Date.now(),
     script: LOCATION_ROOT_URL + "bingo-Client.js?" + Date.now(),
     serverScripts: LOCATION_ROOT_URL + "bingo-Server.js?" + Date.now(),       
     position: Vec3.sum(MyAvatar.position, Vec3.multiplyQbyV(MyAvatar.orientation, { x: 0, y: 1.8, z: -5 })), // { x: 0, y: 1, z: -5 }
